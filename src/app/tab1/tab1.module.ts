7f7c517a884a7d8f92ab74e28d2608df69f7fcc5
Stroescu Marian-Calin <cali_stroescu@yahoo.com>
2021-08-05 21:20:28 +0300

    Initial commit

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,8 +1,9 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { HttpClientModule } from '@angular/common/http';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
@@ -13,8 +14,11 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page],
+  providers: [FormBuilder]
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  submitForm: FormGroup = new FormGroup({
+    text: new FormControl('', Validators.required)
+  }, {updateOn: 'blur'});
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+
+  movies;
+  reviewSubmitted = [];
+  feedbackLogged = [];
+  predictedSentiments = [];
+  randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+  constructor(
+    private http: HttpClient,
+    fb: FormBuilder) {}
+
+  ngOnInit(): void {
+
+    this.http.get('http://127.0.0.1:8000/movies').subscribe(res => {
+      this.movies = res;
+      this.movies.forEach(movie => {
+        this.reviewSubmitted.push(false);
+        this.feedbackLogged.push(false);
+        this.predictedSentiments.push('');
+      });
+    });
+  }
+
+  getMovieTitle(movie): string {
+    return movie.toString().slice(0, -4);
+  }
+
+  setRandomColor() {
+    this.randomColor =  '#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+
+  onSubmit(data, index): void {
+    if(this.submitForm.valid) {
+      this.http.post('http://127.0.0.1:8000/predict', {text: data.value.text}).subscribe((res: any)=>{
+        this.reviewSubmitted[index] = data.value.text;
+        this.predictedSentiments[index] = res;
+
+      });
+    }
+  }
+
+  slideChange(event) {
+    this.submitForm.reset();
+  }
+
+  submitFeedback(is_correct: boolean, index) {
+    this.http.post('http://127.0.0.1:8000/log_feedback',
+     {text: this.reviewSubmitted[index], predicted_sentiment: this.predictedSentiments[index].sentiment, is_correct}).subscribe(
+       res => {
+         this.feedbackLogged[index] = true;
+         this.setRandomColor();
+       }
+     );
+  }
 
 }
